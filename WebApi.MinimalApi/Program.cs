37c9a1beae388b7cefc9ec585215aac097026ff6
Iesht <irinashteynfeld@gmail.com>
2025-09-29 16:36:04 +0500

    GetUserById проходит тесты

--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,10 +1,26 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using WebApi.MinimalApi.Domain;
+using WebApi.MinimalApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
-builder.Services.AddControllers()
+builder.Services.AddControllers(options =>
+    {
+        options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+        options.ReturnHttpNotAcceptable = true;
+        options.RespectBrowserAcceptHeader = true;
+    })
     .ConfigureApiBehaviorOptions(options => {
         options.SuppressModelStateInvalidFilter = true;
         options.SuppressMapClientErrors = true;
     });
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UserDto>();
+    cfg.CreateMap<UserToPostDto, UserEntity>();
+}, Array.Empty<Assembly>());
 
 var app = builder.Build();
 
